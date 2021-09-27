bac41b453e147e1746a9ff898c741d264d2e824c
vladimir <vladimir.obradovic.sa@hotmail.com>
2021-09-27 14:29:15 +0200

    push

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -1,16 +1,22 @@
-   
 const openModal = document.getElementsByClassName('openModal');
 const modal = document.getElementsByClassName('modal');
 const close = document.getElementsByClassName('close');
 
-for (let i=0; i < openModal.length; i++) {
-    openModal[i].addEventListener('click',  () => {
+function DodajEventListener_v1() {
+    for(let i=0; i < openModal.length; i++) {
+        openModal[i].addEventListener('click',  () => {
         modal[i].classList.add('on');
-    })
-};
+        })
+    }
+}
 
-for (let i=0; i < close.length; i++) {
-    close[i].addEventListener('click',  () => {
-        modal[i].classList.remove('on');
-    })
-};
+function DodajEventListener_v2() {
+    for (let i=0; i < close.length; i++) {
+        close[i].addEventListener('click',  () => {
+            modal[i].classList.remove('on');
+        })
+    }
+}
+
+DodajEventListener_v1();
+DodajEventListener_v2();
